fdc1f4835f27cea60c390d6ee7e1c79ea1131aa4
Trực Nguyễn Chánh <chanhtruck13@gmail.com>
2023-03-02 08:03:22 +0700

    Initial commit

---
+++ mmwave/utils/util_functions.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+from threading import Thread, Lock
+
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
+_print = print
+_print_lock = Lock()
+def print(*args, **kwargs):
+    with _print_lock:
+        _print(*args, **kwargs)
+
+# Thread wrapper
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+    return wrapper
+
+def error(msg, **kwargs):
+    print(f'{Fore.RED}{msg}', **kwargs)
+
+def warning(msg, **kwargs):
+    print(f'{Fore.YELLOW}{msg}', **kwargs)
